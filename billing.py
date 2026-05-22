bb73c5b9df360d59bcefb019551f27d87dedb8d9
damerakondasathvika <damarakondasathvika@gmail.com>
2026-05-22 19:38:46 +0530

    Delete billing.py

--- billing.py
+++
@@ -1,9 +0,0 @@
-total=0
-n=int(input("enter number of products:"))
-for i in range(n):
-    product=input("enter product name:")
-    cost=int(input("enter cost of product:"))
-    quantity=int(input("enter number of quantity:"))
-    bill=cost*quantity
-    total=bill+total
-print("Total bill:",total)
