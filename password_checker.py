25df3898d52438866fb467cd061c6c1a40ee6874
damerakondasathvika <damarakondasathvika@gmail.com>
2026-05-22 19:39:18 +0530

    Delete password_checker.py

--- password_checker.py
+++
@@ -1,5 +0,0 @@
-password=input("enter password:")
-if ("@" in password or "#" in password or "$" in password)and(" " not in password):
-    print("Strong password")
-else:
-    print("Weak password")
