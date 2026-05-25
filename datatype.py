81f0b74284180b871d793eda77c8ed02d4a590f8
damerakondasathvika <damarakondasathvika@gmail.com>
2026-05-25 10:00:05 +0530

    Update datatype.py

--- datatype.py
+++ datatype.py
@@ -1,13 +1,26 @@
 x = input("Enter value: ")
 try:
-    x = int(x)
+    x = int(x, 0)
     print("Integer")
 except:
     try:
         x = float(x)
         print("Float")
     except:
-        if x == "True" or x == "False":
+        if x.lower() == "true" or x.lower() == "false":
             print("Boolean")
+        elif x.startswith("[") and x.endswith("]"):
+            print("List")
+        elif x.startswith("(") and x.endswith(")"):
+            print("Tuple")
+        elif x.replace(" ", "") == "{}":
+            print("Dictionary")
+        elif x.startswith("{") and x.endswith("}"):
+            if ":" in x:
+                print("Dictionary")
+            else:
+                print("Set")
+        elif x.lower() == "none":
+            print("NoneType")
         else:
             print("String")
